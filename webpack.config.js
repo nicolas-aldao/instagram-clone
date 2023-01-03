66f885088def5ec74fc1d0f1fd47182f046dbf11
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-01-03 17:42:25 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    output: {
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+}
