b4008c4a6e2c049df184fc0811c11e635ac806db
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-04-23 04:02:02 -0300

    test commit

--- src/components/atoms/Badge/index.js
+++ src/components/atoms/Badge/index.js
@@ -4,7 +4,7 @@ import { BadgeContainer } from './styles';
 export const Badge = ({ content, fontColor, backgroundColor}) => {
   return (
     <BadgeContainer fontColor={fontColor} backgroundColor={backgroundColor}>
-      {content ? content : 'This is a badge!'}
+      {content ? content : 'This is a badge.'}
     </BadgeContainer>
   );
 };
