24ec138008b6852e7dfc812c499edecc40472177
Nicolas Aldao <nicolasaldao@Nicolass-MBP.fibertel.com.ar>
2023-08-28 23:30:07 -0300

    fixing bugs in filter by categories

--- src/components/atoms/Category/index.js
+++ src/components/atoms/Category/index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { Link, Image } from "./styles";
-const DEFAULT_IMAGE = "https://i.imgur.com/dJa0Hpl.jpeg";
+import React from 'react';
+import { Image } from './styles';
+const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpeg';
+const BrokenImage = 'https://static.thenounproject.com/png/504708-200.png';
+const imageOnError = event => {
+  event.currentTarget.src = BrokenImage;
+  event.currentTarget.className = 'error';
+};
 
-export const Category = ({ cover = DEFAULT_IMAGE, path = "#" }) => (
-  // <Link to={path}>
-    <Image src={cover} />
-  // </Link>
+export const Category = ({ cover = DEFAULT_IMAGE }) => (
+  <Image src={cover} onError={imageOnError} />
 );
--- src/components/atoms/Category/styles.js
+++ src/components/atoms/Category/styles.js
@@ -1,13 +1,4 @@
 import styled from "styled-components";
-import { Link as LinkRouter } from 'react-router-dom'
-
-export const Link = styled(LinkRouter)`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  text-decoration: none;
-  width: 75px;
-`;
 
 export const Image = styled.img`
     border: 1px solid #ddd;
@@ -18,4 +9,5 @@ export const Image = styled.img`
     object-fit: cover;
     height: 75px;
     width: 75px;
+    cursor: pointer;
 `;
