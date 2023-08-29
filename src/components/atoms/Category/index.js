8398a3b12e5a8dfbbaf166970e5a7c33b75d918c
Nicolas Aldao <nicolasaldao@Nicolass-MBP.fibertel.com.ar>
2023-08-28 23:52:18 -0300

    handling error when image doesn't load

--- src/components/atoms/Category/index.js
+++ src/components/atoms/Category/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Image } from './styles';
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpeg';
-const BrokenImage = 'https://static.thenounproject.com/png/504708-200.png';
+const BrokenImage =
+  'https://developers.google.com/static/maps/documentation/maps-static/images/error-image-generic.png';
 const imageOnError = event => {
   event.currentTarget.src = BrokenImage;
-  event.currentTarget.className = 'error';
+  event.currentTarget.style.maxWidth = '75px';
+  event.currentTarget.style.maxHeight = '75px';
+  event.currentTarget.style.objectFit = 'none';
 };
 
 export const Category = ({ cover = DEFAULT_IMAGE }) => (
