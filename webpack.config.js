1b774385c14fbeeac1858e55fb873d166416d86e
Nicolás Aldao <nicolasaldao@live.com>
2024-05-07 20:50:08 -0300

    no funciona lpm

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
