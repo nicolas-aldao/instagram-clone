ec990128bcaff3083b402dff5f63f6cefe70821f
Nicolás Aldao <nicolasaldao@live.com>
2024-05-07 20:36:31 -0300

    testing favicon 18

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      favicon: './public/favicon.ico',
     }),
     new WebpackPwaManifestPlugin({
       name: 'Artgram',
